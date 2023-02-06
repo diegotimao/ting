2c13ab31c263612c6b8b0b5552c16748699c3862
diegotimao <diegotimao1104@gmail.com>
2023-02-06 17:23:50 -0500

    feat: Implementando a função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    notice = txt_importer(path_file)
+
+    for item in instance.data:
+        if path_file in item["nome_do_arquivo"]:
+            return None
+
+    file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(notice),
+        "linhas_do_arquivo": notice,
+    }
+
+    instance.enqueue(file)
+
+    return sys.stdout.write(str(file))
 
 
 def remove(instance):
