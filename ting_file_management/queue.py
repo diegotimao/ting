28cbe743a53c852beffdf888588411f0c9b5d9c6
diegotimao <diegotimao1104@gmail.com>
2023-02-06 12:12:02 -0500

    feat: Implementando a classe Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,24 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self.data) == 0:
+            return None
+
+        return self.data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        size = len(self.data)
+
+        if not index >= 0 and index <= size:
+            raise IndexError
+        
+        return self.data[index]
