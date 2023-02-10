e36a3e66bfc29e1274a322a5254484e97a0605b9
diegotimao <diegotimao1104@gmail.com>
2023-02-10 13:33:55 -0500

    feat: Implementando a função que busca se uma palavra passada existe em algum dos items da fila

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá su implementação"""
+    data = instance.data
+    list_words = []
+    list_currencyes = []
+
+    for item_data in data:
+        for index, item in enumerate(item_data["linhas_do_arquivo"]):
+            if word.lower() in item.lower():
+                list_words.append({"linha": index + 1})
+
+        if len(list_words) > 0:
+            list_currencyes.append(
+                {
+                    "palavra": word,
+                    "arquivo": item_data["nome_do_arquivo"],
+                    "ocorrencias": list_words,
+                }
+            )
+    return list_currencyes
 
 
 def search_by_word(word, instance):
